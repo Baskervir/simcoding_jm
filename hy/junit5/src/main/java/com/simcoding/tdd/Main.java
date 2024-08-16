fd21ada0302e2090bf4a51e89d6c3b1c1cecf95a
Juneseo <lookhkh37@gmail.com>
2024-08-16 17:00:07 +0900

    base project 추가

--- hy/junit5/src/main/java/com/simcoding/tdd/Main.java
+++ hy/junit5/src/main/java/com/simcoding/tdd/Main.java
@@ -1,10 +1,8 @@
 package com.simcoding.tdd;
 
-import com.simcoding.tdd.calculator.Calculator;
-
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println(1);
     }
 }
--- hy/tdd/src/main/java/com/simcoding/tdd/calculator/Calculator.java
+++
@@ -1,8 +0,0 @@
-package com.simcoding.tdd.calculator;
-
-public class Calculator {
-
-    public long sum(long left, long right) {
-        return left + right;
-    }
-}
