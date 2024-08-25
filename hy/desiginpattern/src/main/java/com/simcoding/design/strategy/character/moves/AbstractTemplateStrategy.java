a34e81d06d31b5f34f10b9c0bd92f89a34bcc805
Juneseo <lookhkh37@gmail.com>
2024-08-25 19:33:14 +0900

    주의 메시지 추가

--- hy/desiginpattern/src/main/java/com/simcoding/design/strategy/character/moves/AbstractTemplateStrategy.java
+++ hy/desiginpattern/src/main/java/com/simcoding/design/strategy/character/moves/AbstractTemplateStrategy.java
@@ -6,7 +6,7 @@ import com.simcoding.design.strategy.character.Direction;
 /**
  * @apiNote 공통된 코드를, 부모 abstract 클래스에 모아두고,
  * 변경되는 부분만, 하위 클래스에서 overide하여 처리한다.
- *
+ * @apiNote 상속을 사용할 경우, 부모 클래스에 종속되기 때문에, 꼭 필요한 경우에만 사용할 수 있도록 한다.
  * **/
 public abstract class AbstractTemplateStrategy implements MoveStrategy{
     @Override
