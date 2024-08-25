87dc5851c6471c2cbd88fa77776f529a14bf44ac
Juneseo <lookhkh37@gmail.com>
2024-08-25 19:19:09 +0900

    report decorator 추가

--- hy/desiginpattern/src/main/java/com/simcoding/design/strategy/Main.java
+++ hy/desiginpattern/src/main/java/com/simcoding/design/strategy/Main.java
@@ -4,6 +4,7 @@ import com.simcoding.design.strategy.character.Direction;
 import com.simcoding.design.strategy.character.MainCharacter;
 import com.simcoding.design.strategy.character.moves.CarStrategy;
 import com.simcoding.design.strategy.character.moves.FootStretegy;
+import com.simcoding.design.strategy.character.moves.ReportDecorator;
 
 /**
  * @apiNote 캐릭터를 이동시키는 어플리케이션이 있다.
@@ -14,16 +15,16 @@ public class Main {
     public static void main(String[] args) {
 
         // 캐릭터 시뮬레이션 어플리케이션
-        MainCharacter character = new MainCharacter(new FootStretegy());
+        MainCharacter character = new MainCharacter(new ReportDecorator(new FootStretegy()));
         character.move(Direction.UP);
         character.move(Direction.RIGHT);
         character.speakWhereIam();
-        character.setTransport(new CarStrategy());
+        character.setTransport(new ReportDecorator(new CarStrategy()));
         //클라이언트 코드(MainChaacter)를 변경하지 않고, 기능을 확장
         character.move(Direction.UP);
         character.speakWhereIam();
 
-        
+
 
     }
 }
---
+++ hy/desiginpattern/src/main/java/com/simcoding/design/strategy/character/moves/ReportDecorator.java
@@ -0,0 +1,32 @@
+package com.simcoding.design.strategy.character.moves;
+
+import com.simcoding.design.strategy.character.Direction;
+
+/**
+/
+ 기능을 확장하고자 하는 클래스의 인터페이스를 상속받는다.
+
+/ **/
+public class ReportDecorator implements MoveStrategy{
+    private final MoveStrategy component;
+
+    public ReportDecorator(MoveStrategy component) {
+        this.component = component;
+    }
+
+    @Override
+    public int moveY(Direction dir, int curY) {
+        System.out.println("--------- 현재 위치는 "+curY);
+        int nextY = this.component.moveY(dir, curY);
+        System.out.println("--------- 다음 위치는 "+nextY);
+        return nextY;
+    }
+
+    @Override
+    public int moveX(Direction dir, int curX) {
+        System.out.println("---------  현재 위치는 "+curX);
+        int nextX = this.component.moveX(dir, curX);
+        System.out.println("---------  다음 위치는 "+nextX);
+        return nextX;
+    }
+}
