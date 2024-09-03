e23dadd331d95cb40bcf8b90cef2a3097d24f56d
hy <lookhkh37@gmail.com>
2024-09-03 23:11:05 +0900

    template

--- hy/datastructure/src/main/java/com/simcoding/TestExecutor.java
+++ hy/datastructure/src/main/java/com/simcoding/TestExecutor.java
@@ -1,14 +1,54 @@
 package com.simcoding;
 
-import org.junit.jupiter.api.Test;
+import java.time.Duration;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
 
 public class TestExecutor {
 
-    static public void timeLaps(Runnable r){
+    static public <T> void add100_000_00Elements(List<Integer> list){
+        logTimeLaps("add", ()->
+            IntStream.range(0, 10_000_000).forEach(
+                    list::add
+            )
+        );
+    }
+
+    static public <T> void removeRandomIndexFor1000Times(List<Integer> list){
+        logTimeLaps("remove", ()->IntStream.range(0, 1_000).forEach(
+                list::remove
+        ));
+    }
+
+    static public <T> void accRandomElementFor10000Times(List<Integer> list){
+        logTimeLaps("random access",()->
+            IntStream.range(0, 10_000).forEach(
+                    t-> {
+                        Integer i = list.get(t);
+                    }
+            )
+        );
+    }
+
+
+    static public <T> void insertElementAtRandomIndexFor10_000Times(List<Integer> list){
+        logTimeLaps("insert",()->
+            IntStream.range(0, 10_000).forEach(
+                    t-> {
+                        Random r = new Random();
+                        list.add(r.nextInt(10_000), t);
+                    }
+            )
+        );
+    }
+
+    static private void logTimeLaps(String marker, Runnable r){
         long start = System.currentTimeMillis();
         r.run();
-        System.out.print(System.currentTimeMillis());
         long end = System.currentTimeMillis() - start;
+        Duration duration = Duration.ofMillis(end);
+        System.out.println("timelaps : "+ duration.getSeconds()+" 초"+" "+duration.toMillis());
     }
 
 }
--- hy/datastructure/src/main/java/com/simcoding/datastructur/linear/ArrayDataStructur.java
+++ hy/datastructure/src/main/java/com/simcoding/datastructur/linear/ArrayDataStructur.java
@@ -1,2 +1,18 @@
-package com.simcoding.datastructur.linear;public class ArrayDataStructur {
+package com.simcoding.datastructur.linear;
+
+import com.simcoding.TestExecutor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayDataStructur {
+
+    public static void main(String[] args) {
+
+        List<Integer> arrayList = new ArrayList<>();
+        TestExecutor.add100_000_00Elements(arrayList);
+        TestExecutor.removeRandomIndexFor1000Times(arrayList);
+
+    }
+
 }
