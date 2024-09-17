77a07a21396ba799f890f8177b1dadbfa22133bb
hy <lookhkh37@gmail.com>
2024-09-17 23:43:50 +0900

    multithread project 생성

---
+++ hy/multithread/src/test/java/com/simcoding/tdd/MainTest.java
@@ -0,0 +1,17 @@
+package com.simcoding.tdd;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    @DisplayName("hi")
+    public void t(){
+        Assertions.assertEquals(1, 1);
+    }
+
+}
